0fd48b13769382dc2f718f358354cf439b0e8969
Mihai Gabriel <17096004+mihai-gabriel@users.noreply.github.com>
2021-10-24 13:58:56 +0300

    Refactoring Game into Components

--- src/Game.js
+++ src/Game.js
@@ -3,35 +3,49 @@ import axios from "axios";
 import './Game.css';
 
 
-function contains(arr, item) {
-    let itemString = JSON.stringify(item);
-
-    return arr.some(el => {
-        return JSON.stringify(el) === itemString;
-    });
+function GameOver(props) {
+    return (
+        <div className={props.isWon ? "gameStatus" : "hidden"}>
+            <h2>Congrats, you won</h2>
+        </div>
+    )
 }
 
-class Game extends Component {
+function Pocket(props) {
+    return (
+        <div className="pocket">
+            <div>
+                <span className={props.zeros === 0 ? "empty" : ""}>{props.zeros} zeros left</span>
+            </div>
+            <div className="break" />
+            <div>
+                <span className={props.ones === 0 ? "empty" : ""}>{props.ones} ones left</span>
+            </div>
+        </div>
+    )
+}
+
+function ResetButton(props) {
+    return (
+        <div className="gameReset">
+            <button onClick={() => props.onClick()}>Reset game</button>
+        </div>
+    )
+}
+
+function SwitchButton(props) {
+    return (
+        <button
+            onClick={() => {props.onClick(props.switchValue)}}
+            className={props.value === props.switchValue ? "active" : ""}
+        >{props.switchValue}
+        </button>
+    )
+}
+
+class Board extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            immutable: [],
-            pocket: {
-                0: 0,
-                1: 0,
-            },
-            value: 1,
-            initialFen: '',
-            initialPocket: {},
-            board: [
-                ['-', '-', '-', '-', '-', '-'],
-                ['-', '-', '-', '-', '-', '-'],
-                ['-', '-', '-', '-', '-', '-'],
-                ['-', '-', '-', '-', '-', '-'],
-                ['-', '-', '-', '-', '-', '-'],
-                ['-', '-', '-', '-', '-', '-'],
-            ]
-        }
         this.mappingLines = {
             0: '6',
             1: '5',
@@ -50,6 +64,76 @@ class Game extends Component {
         }
     }
 
+    getCoord(i, j) {
+        return this.mappingColumns[j] + this.mappingLines[i];
+    }
+
+    renderColumn(i, j) {
+        let isPreset = contains(this.props.immutable, [i, j]);
+        let boardValue = this.props.board[i][j];
+        let displayValue = boardValue !== '-' ? boardValue : " ";
+        let coord = this.getCoord(i, j);
+
+        return (
+            <td
+                key={coord}
+                onClick={() => this.props.updateBoard(i, j)}
+                className={isPreset ? "preset" : ""}
+            >{displayValue}</td>
+        );
+    }
+
+    renderRow(i) {
+        let columns = [];
+        for (let j = 0; j < 6; j++) {
+            columns.push(this.renderColumn(i, j));
+        }
+
+        return (
+            <tr key={this.mappingColumns[i]}>
+                {columns}
+            </tr>
+        )
+    }
+
+    render() {
+        let rows = [];
+        for (let i = 0; i < 6; i++) {
+            rows.push(this.renderRow(i));
+        }
+
+        return (
+            <table className="gameBoard">
+                <tbody>
+                {rows}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.initialGame = {};
+        this.state = {
+            immutable: [],
+            pocket: {
+                0: 0,
+                1: 0,
+            },
+            value: 1,
+            board: [
+                ['-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-'],
+            ]
+        }
+    }
+
     fen() {
         let result = "";
         this.state.board.forEach(row => {
@@ -61,33 +145,33 @@ class Game extends Component {
     loadFen() {
         let result = [];
         let immutable = [];
-        this.state.initialFen.split('/').forEach(row => {
-            result.push(row.split(""));
-        });
+        this.initialGame.fen.split('/').forEach((row, i) => {
+            let rowSplit = row.split('');
+            result.push(rowSplit);
 
-        for (let i = 0; i < 6; i++) {
-            for (let j = 0; j < 6; j++) {
-                if (result[i][j] !== '-') {
+            rowSplit.forEach((column, j) => {
+                if (column !== '-') {
                     immutable.push([i, j]);
                 }
-            }
-        }
+            });
+        });
         this.setState({board: result, immutable: immutable});
     }
 
     async fetchPreset() {
         let response = await axios.get('http://127.0.0.1:5000/api/v1/resources/game');
+        this.initialGame.fen = response.data['fen'];
+        this.initialGame.pocket = response.data['pocket'];
+
         this.setState({
-            initialFen: response.data['fen'],
             pocket: response.data['pocket'],
-            initialPocket: response.data['pocket'],
             winning: response.data['winning'],
         });
         this.loadFen();
     }
 
     resetGame() {
-        this.setState({pocket: this.state.initialPocket});
+        this.setState({pocket: this.initialGame.pocket});
         this.loadFen();
     }
 
@@ -97,22 +181,24 @@ class Game extends Component {
         let currentValue = this.state.value;
         let oppositeValue = Math.abs(this.state.value - 1); // abs(0 - 1) = 1 || abs(1 - 1) = 0
 
-        if (contains(this.state.immutable, [i, j]) || this.state.pocket[currentValue] === 0) {
+        if (contains(this.state.immutable, [i, j]) ||
+            this.state.pocket[currentValue] === 0) {
             return;
         }
 
         if (currentValue === 'take') {
-            if (!isNaN(oldValue)) {
-                let oppositeOldValue = Math.abs(oldValue - 1);
-                currentBoard[i][j] = "-";
-                this.setState({
-                    board: currentBoard,
-                    pocket: {
-                        [oldValue]: this.state.pocket[oldValue] + 1,
-                        [oppositeOldValue]: this.state.pocket[oppositeOldValue]
-                    }
-                });
+            if (isNaN(oldValue)) {
+                return;
             }
+
+            let newPocket = this.state.pocket;
+            newPocket[oldValue] = this.state.pocket[oldValue] + 1;
+            currentBoard[i][j] = "-";
+            this.setState({
+                board: currentBoard,
+                pocket: newPocket
+            });
+
             return;
         }
 
@@ -127,99 +213,58 @@ class Game extends Component {
         this.setState({board: currentBoard});
     }
 
-    getCoord(i, j) {
-        return this.mappingColumns[j] + this.mappingLines[i];
-    }
-
-    renderSquare(i, j) {
-        let isPreset = contains(this.state.immutable, [i, j]);
-        let value = this.state.board[i][j] !== '-' ? this.state.board[i][j] : " ";
-        let coord = this.getCoord(i, j);
-
-        return (
-            <td
-                key={coord}
-                onClick={() => this.updateBoard(i, j)}
-                className={isPreset ? "preset" : ""}
-            >{value}</td>
-        );
-    }
-
-    renderRow(i) {
-        let columns = [];
-        for (let j = 0; j < 6; j++) {
-            columns.push(this.renderSquare(i, j));
-        }
-
-        return (
-            <tr key={this.mappingColumns[i]}>
-                {columns}
-            </tr>
-        )
-    }
-
-    renderBoard() {
-        let rows = [];
-        for (let i = 0; i < 6; i++) {
-            rows.push(this.renderRow(i));
-        }
-
-        return (
-            <table className="gameBoard">
-                <tbody>
-                    {rows}
-                </tbody>
-            </table>
-        )
+    setValue(value) {
+        this.setState({value: value});
     }
 
     componentDidMount() {
         this.fetchPreset();
     }
 
+    renderSwitchButton(value) {
+        return (
+            <SwitchButton
+                switchValue={value}
+                onClick={this.setValue.bind(this)}
+                value={this.state.value}
+            />
+        )
+    }
+
     render() {
         let won = this.state.winning === this.fen();
 
         return (
-            <>
-                <div className={won ? "gameStatus": "hidden"}>
-                    <h2>Congrats, you won</h2>
-                </div>
-                <div className="pocket">
-                    <div>
-                        <span>{this.state.pocket[0]} zeros left</span>
-                    </div>
-                    <div className="break"/>
-                    <div>
-                        <span>{this.state.pocket[1]} ones left</span>
-                    </div>
-                </div>
-                <div className="gameControls">
-                    <button onClick={() => this.resetGame()}>Reset game</button>
-                </div>
+            <div className="game">
+                <GameOver isWon={won}/>
+                <Pocket
+                    zeros={this.state.pocket[0]}
+                    ones={this.state.pocket[1]}
+                />
+                <ResetButton onClick={this.resetGame.bind(this)}/>
                 <div className="gameBoard">
-                    {this.renderBoard()}
+                    <Board
+                        immutable={this.state.immutable}
+                        board={this.state.board}
+                        updateBoard={this.updateBoard.bind(this)}
+                    />
                 </div>
                 <div className="switchButtons">
-                    <button
-                        onClick={() => {this.setState({value: 1})}}
-                        className={this.state.value === 1 ? "active" : ""}
-                    >1
-                    </button>
-                    <button
-                        onClick={() => {this.setState({value: 0})}}
-                        className={this.state.value === 0 ? "active" : ""}
-                    >0
-                    </button>
-                    <button
-                        onClick={() => {this.setState({value: 'take'})}}
-                        className={this.state.value === 'take' ? "active" : ""}
-                    >Take</button>
+                    {this.renderSwitchButton(1)}
+                    {this.renderSwitchButton(0)}
+                    {this.renderSwitchButton('take')}
                 </div>
-            </>
-
+            </div>
         )
     }
+}
+
+function contains(arr, item) {
+    let itemString = JSON.stringify(item);
+
+    return arr.some(el => {
+        return JSON.stringify(el) === itemString;
+    });
 }
 
 export default Game;
